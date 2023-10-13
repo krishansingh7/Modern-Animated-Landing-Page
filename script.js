5fcb11354c28b588d8705b0d09f35ee196148f8e
Krishan singh <115705643+krishansingh7@users.noreply.github.com>
2023-10-14 03:12:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+Shery.imageEffect(".back", {
+  style: 5,
+  config: {
+    a: { value: 1.98, range: [0, 30] },
+    b: { value: -0.97, range: [-1, 1] },
+    aspect: { value: 2.0721966205837172 },
+    gooey: { value: true },
+    infiniteGooey: { value: true },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.26, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: false },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.5, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.83, range: [0, 1] },
+    antialias_threshold: { value: 0.08, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+  gooey: true,
+//   debug: true,
+});
+
+let elems = document.querySelectorAll(".elem");
+const main = document.querySelector(".main");
+
+elems.forEach((el)=>{
+
+    const h2s = el.querySelectorAll("h2");
+    let index = 0;
+    main.addEventListener("click",()=>{
+        gsap.to(h2s[index],{
+            top: "-=100%",
+            ease: Expo.easeInOut,
+            duration: 1,
+            onComplete: function(){
+                gsap.set(this._targets[0],{top: "100%"});
+            },
+        });
+    
+        index === h2s.length-1 ? (index=0) : index++;
+    
+        gsap.to(h2s[index],{
+            top: "-=100%",
+            ease: Expo.easeInOut,
+            duration: 1,
+        });
+    });
+
+})
+
